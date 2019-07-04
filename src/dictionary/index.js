d085abdbfa48056bf963071690d2e23b7f8cabda
jun.zhua <jun.zhua@ctrip.com>
2019-07-04 19:09:01 +0800

    release 1.0.1(6.5 times performance improvement)

---
+++ src/dictionary/index.js
@@ -0,0 +1,17 @@
+import dictionary1 from './dictionary_1'
+import dictionary2 from './dictionary_2'
+import dictionary3 from './dictionary_3'
+import dictionary4 from './dictionary_4'
+import dictionary5 from './dictionary_5'
+import dictionary6 from './dictionary_6'
+import surnames from './surnames'
+
+export default {
+  ...dictionary1,
+  ...dictionary2,
+  ...dictionary3,
+  ...dictionary4,
+  ...dictionary5,
+  ...dictionary6,
+  ...surnames
+}
--- src/index.js
+++ src/index.js
@@ -1,43 +1,19 @@
-import dictionary1 from './dictionary/dictionary_1'
-import dictionary2 from './dictionary/dictionary_2'
-import dictionary3 from './dictionary/dictionary_3'
-import dictionary4 from './dictionary/dictionary_4'
-import dictionary5 from './dictionary/dictionary_5'
-import dictionary6 from './dictionary/dictionary_6'
-import surnames from './dictionary/surnames'
+import dictionary from './dictionary'
 import { removeTone, convertToneToNumber } from './utils'
 
 const REGEXP = /[\u4E00-\u9FA5]/g
 
-const dictionarys = [
-  dictionary1,
-  dictionary2,
-  dictionary3,
-  dictionary4,
-  dictionary5,
-  dictionary6,
-  surnames
-]
+const keys = Object.keys(dictionary)
 
 export default (str, options = {}) => {
   let result = str.match(REGEXP).join('')
 
-  let i = 0
-  let lens = dictionarys.length
-  for (; i < lens; i += 1) {
-    let complete = false
-    for (let key of Object.keys(dictionarys[i])) {
-      if (~result.indexOf(key)) {
-        result = result.replace(new RegExp(key, 'g'), dictionarys[i][key])
-        if (!REGEXP.test(result)) {
-          complete = true
-          break
-        }
+  for (let key of keys) {
+    if (~result.indexOf(key)) {
+      result = result.replace(new RegExp(key, 'g'), dictionary[key])
+      if (!REGEXP.test(result)) {
+        break
       }
-    }
-
-    if (complete) {
-      break
     }
   }
 
